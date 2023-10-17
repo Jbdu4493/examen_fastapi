8d5cdef2ed128e7f641823035de1ef8f5d677bf2
Jbdu4493 <62330512+Jbdu4493@users.noreply.github.com>
2023-10-17 16:20:09 +0200

    l'api tourne

--- main.py
+++ main.py
@@ -1,18 +1,68 @@
-from fastapi import FastAPI, Header, HTTPException
-from sqlmodel import Session, SQLModel, create_engine, select
+from fastapi import FastAPI, HTTPException,Header
+from pydantic import BaseModel
+from typing import Optional
+import json
+import pandas as pd 
 
-import os
-from controleur import Contoleur,Question,User
+def create_db():
+    df =  pd.read_csv("https://dst-de.s3.eu-west-3.amazonaws.com/fastapi_fr/questions.csv")
+    df = df[~ df.correct.isna()]
+    df.correct = df.correct.str.replace(',',' ')
+    df.correct = df.correct.apply(lambda x: x.split(" "))
+    return df
 
-app = FastAPI()
+base_de_donnee = create_db()
 
-controle = Contoleur()
+users = { 
+  "alice": "wonderland",
+  "bob": "builder",
+  "clementine": "mandarine",
+  "admin" : "4dm1N"
+}
+class Question(BaseModel):
+    question: str
+    subject: str
+    use: str
+    correct: list[str]
+    responseA: str
+    responseB: str
+    responseC: Optional[str] = None
+    responseD:Optional[str] = None
+    remark: Optional[str] = None
     
 
-@app.post("/questions/", name = "Creation du utilis")
-def create_question(Authorization:Header(),question: Question):
-    return 
+api = FastAPI(title="My QCM",
+    description="Examen FastAPI",
+    version="1.0.1")
+    
 
-@app.post("/users/",name="Creation d'un utilisateur")
-def create_question(Authorization:Header(),user: User):       
-        return 
+@api.post("/questions",
+          name = "Creation d'un question",
+          responses ={200: {"description": "OK"},
+                     409: {"description": "No allow to add question"}})
+def create_question(question: Question,Authorization:str = Header()):
+    """Fonction permettant a un admin de cree une question"""
+    return {"detail":'OK'}
+
+@api.get("/questions",name = "Retourn tout les questions disponible",
+          responses ={200: {"description": "OK"},
+                     403: {"description": "No allow to get all questions"}})
+def get_all(question: Question,Authorization:str = Header()):
+    """""Fonction permettant a un admin de voir toute les questions disponible """""
+    return {"detail":'OK'}
+
+
+@api.get("/questions/use",name = "Retourn tout les questions disponible",
+          responses ={200: {"description": "OK"},
+                     403: {"description": "No allow to get question" },
+                     406: {"description": "Not enougth questions" }})
+def get_qcm_by_use(use: str, nb_question:int,Authorization:str = Header()):
+    return {"detail":'OK'}
+
+
+@api.get("/questions/subjects",name = "Retourn tout les questions disponible",
+          responses ={200: {"description": "OK"},
+                     403: {"description": "No allow to get question" },
+                     406: {"description": "Not enougth questions" }})
+def get_qcm_by_subjects(subjects: list[str], nb_question:int,Authorization:str = Header()):
+    {"detail":'OK'}
