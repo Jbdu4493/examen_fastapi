6d539d439eb6ec21ff7e000b3d90573ffb5684f3
Jbdu4493 <62330512+Jbdu4493@users.noreply.github.com>
2023-10-19 22:48:02 +0200

    update install.sh requiement

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 #/bin/bash
-
-python3 -m venv venv_examen_fastapi
-source ./venv_examen_fastapi/bin/activate
-
 pip install --upgrade pip
-
+pip3 install httptools==0.1.* uvloop, streamlit
 pip install -r requirements.txt
-
-sudo apt-get install uvicorn
-
-uvicorn main:api --reload &git 
-streamlit run st_test_api.py &
+sudo apt-get install uvicorn &
+uvicorn main:api --reload &
