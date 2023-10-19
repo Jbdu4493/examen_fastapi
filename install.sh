130a07b3bd6f1bd0c9e9b1506f17157bb08ac4ab
Jbdu4493 <62330512+Jbdu4493@users.noreply.github.com>
2023-10-19 22:29:41 +0200

    update source commande

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #/bin/bash
 
 python3 -m venv venv_examen_fastapi
-source venv_examen_fastapi/bin/activate
+source ./venv_examen_fastapi/bin/activate
+
 pip install --upgrade pip
+
 pip install -r requirements.txt
+
 sudo apt-get install uvicorn
-uvicorn main:api --reload &
+
+uvicorn main:api --reload &git 
 streamlit run st_test_api.py &
