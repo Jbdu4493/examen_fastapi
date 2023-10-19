418d76d0392d5d6d03ea7712a7e612852e952e23
Jbdu4493 <62330512+Jbdu4493@users.noreply.github.com>
2023-10-19 22:00:02 +0200

    install.sh + requirements

---
+++ install.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+python3 -m venv venv_examen_fastapi
+source venv_examen_fastapi/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+sudo apt-get install uvicorn
+uvicorn main:api --reload &
+streamlit run st_test_api.py &
